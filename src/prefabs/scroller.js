0e26c0492c92ddd374c68f014eb4bd5709e0fd31
Christian Caspers <ccaspers@users.noreply.github.com>
2016-10-14 19:13:27 +0200

    make scroller more configurable, disable changes to world-bounds by player

--- src/prefabs/scroller.js
+++ src/prefabs/scroller.js
@@ -1,27 +1,31 @@
 class Scroller extends Phaser.Group {
 
-  constructor (game, tileId, name, offsetX, offesetY) {
+  constructor (game, tileId, name, scrollspeed, tileHeight, tileWidth, align) {
     // this.game, parent, name, addToStage, enableBody, physicsBodyType
     super(game, undefined, name, false, true, Phaser.Physics.ARCADE);
-    this.tileWidth = game.world.width;
-    this.tileHeight = game.world.height;
-    this.createMultiple(3, tileId);
+    this.tileWidth = tileWidth;
+    this.tileHeight = tileHeight;
+    this.scrollspeed = scrollspeed;
+    var neededTiles = Math.floor(game.world.height / tileHeight) + 2;
+    console.log("needing many tiles:" , neededTiles);
+    this.createMultiple(neededTiles, tileId);
 
     for(var i = 0; i < this.children.length; i++) {
       var tile = this.children[i];
       var height = game.world.height;
       tile.events.onKilled.add(function(tile){
-        this.initTile(tile, 0, - height);
+        this.initTile(tile, 0, - tileHeight);
       }, this);
-      this.initTile(tile, 0, height - i * height);
+      console.log("init tile with y: ", i * tileHeight - tileHeight);
+      this.initTile(tile, 0, i * tileHeight - tileHeight);
     }
   }
 
   initTile(tile, x, y){
-      tile.width = this.game.world.width;
-      tile.height = this.game.world.height;
+      tile.width = this.tileWidth;
+      tile.height = this.tileHeight;
       tile.reset(x, y);
-      tile.body.velocity.y = 150;
+      tile.body.velocity.y = this.scrollspeed;
       tile.body.immovable = true;
       tile.body.acceleration = new Phaser.Point();
       tile.checkWorldBounds = true;
@@ -29,4 +33,5 @@ class Scroller extends Phaser.Group {
   }
 
 }
+
 export default Scroller;
