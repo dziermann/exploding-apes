9700f964fed14a2ac96a27e44ad8d28eb99640a6
Christian Caspers <ccaspers@users.noreply.github.com>
2016-10-14 11:31:14 +0200

    remove unnecessary import

--- src/states/game.js
+++ src/states/game.js
@@ -1,7 +1,6 @@
 import Player from '../prefabs/player';
 import DebugPlayer from '../prefabs/player-debug';;
 import Platform from '../prefabs/platform';
-import Explosion from '../prefabs/explosion';
 
 var bricks;
 
