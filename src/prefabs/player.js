e479bedef2484ccbbb7519264de34d83104a760b
Christian Caspers <ccaspers@users.noreply.github.com>
2016-10-14 11:32:58 +0200

    reset player to version before rebase

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -3,9 +3,10 @@ import Explosion from '../prefabs/explosion';
 class Player extends Phaser.Sprite {
 
   //initialization code in the constructor
-  constructor(game, x, y, frame) {
-    super(game, x, y, 'player', frame);
+  constructor(game, x, y, sprite, control) {
+    super(game, x, y, sprite, 0);
 
+    this.control = control;
     //setup physics properties
     this.anchor.setTo(0.5, 0.5);
     this.game.physics.arcade.enableBody(this);
@@ -15,12 +16,9 @@ class Player extends Phaser.Sprite {
     this.animations.add('right', [1, 2, 3, 2], 3, true);
     this.animations.add('left', [9, 8, 7, 8], 3, true);
     this.animations.add('idle', [0, 5], 1, false);
-    this.animations.add('jump-right', [0, 5], 1, false);
-    this.animations.add('jump-left', [0, 5], 1, false);
-    this.animations.add('jump', [0, 5], 1, false);
-
-	//setup audio
-    this.sparkle = this.game.add.audio('sparkle');
+    this.animations.add('jump-right', [4], 1, false);
+    this.animations.add('jump-left', [6], 1, false);
+    this.animations.add('jump', [10], 1, false);
 
     //set size
     this.scale.setTo(2, 2);
@@ -40,56 +38,40 @@ class Player extends Phaser.Sprite {
 
   update() {
     this.body.velocity.x = 0;
+    var cursorLeft = this.cursors.left.isDown;
+    var cursorRight = this.cursors.right.isDown;
+    var controllerLeft = this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1;
+    var controllerRight = this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1;
+    var spacebarDown = this.spacebar.isDown;
+    var controllerDown = this.pad1.justPressed(Phaser.Gamepad.XBOX360_A);
 
 
-    if (this.cursors.left.isDown) {
+
+    if ((this.control === 'keyboard' && cursorLeft) || (this.control === 'controller' && controllerLeft)) {
       this.body.velocity.x = -400;
-      this.animations.play('left');
-    } else if (this.cursors.right.isDown) {
+      if (this.body.velocity.y !== 0) {
+        this.animations.play('jump-left');
+      } else {
+        this.animations.play('left');
+      }
+
+    } else if ((this.control === 'keyboard' && cursorRight) || (this.control === 'controller' && controllerRight)) {
       this.body.velocity.x = 400;
-      this.animations.play('right');
+      if (this.body.velocity.y !== 0) {
+        this.animations.play('jump-right');
+      } else {
+        this.animations.play('right');
+      }
     } else {
       this.body.velocity.x = 0;
-	  this.animations.play('idle');
+      this.animations.play('idle');
     }
 
-
-    if (this.spacebar.isDown && this.body.velocity.y === 0) {
+    if (this.body.velocity.y === 0 && (this.control === 'keyboard' && spacebarDown) || (this.control === 'controller' && controllerDown)) {
       this.body.velocity.y = -600;
-      this.sparkle.play();
+      this.animations.play('jump');
       this.explosion = new Explosion(this.game, this.body.position.x, this.body.position.y, '', 'banana');
       this.game.add.existing(this.explosion);
-    }
-
-
-
-    // CONTROLLER INPUT
-    if (this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1) {
-      this.body.velocity.x = -400;
-      this.animations.play('left');
-    } else if (this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1) {
-      this.body.velocity.x = 400;
-      this.animations.play('right');
-    }
-
-    if (this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1 && this.body.velocity.y == 0) {
-      this.body.velocity.y = -600;
-    } else if (this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1) {
-      console.log('down');
-    }
-
-    if (this.pad1.justPressed(Phaser.Gamepad.XBOX360_A)) {
-      this.explosion = new Explosion(this.game, this.body.position.x, this.body.position.y, '', 'banana');
-      this.game.add.existing(this.explosion);
-    }
-    if (this.pad1.justPressed(Phaser.Gamepad.XBOX360_X)) {
-      console.log('X');
-    }
-    if (this.pad1.justPressed(Phaser.Gamepad.XBOX360_Y)) {
-      console.log('Y');
-    }
-    if (this.pad1.justPressed(Phaser.Gamepad.XBOX360_B)) {
-      console.log('B');
     }
 
   }
