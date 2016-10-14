e84b06d925f9db1253eead6035100d93b33cd577
Christian Caspers <ccaspers@users.noreply.github.com>
2016-10-14 11:30:11 +0200

    disable antialising because performance

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import Menu from './states/menu';
 import Background from './states/background';
 import Preloader from './states/preloader';
 
+// new Game(width, height, renderer, parent, state, transparent, antialias, physicsConfig)
+
 
 const game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'exploding-apes-game');
 
+game.antialias = false;
 game.state.add('boot', new Boot());
 game.state.add('preloader', new Preloader());
 game.state.add('background', new Background());
